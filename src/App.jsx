2021e47a0fe60fcc30e6355a756a4f2b91081ec8
Eduardo Tumimbo <eduardotumimbo.yns@gmail.com>
2023-06-20 23:09:57 +0800

    Basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
-function MyButton() {
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function NavBarTop() {
   return (
-    <button>I'm a button</button>
+    <>
+      <Navbar className="bg-danger p-3">
+        <Container>
+          <Row>
+            <Col>
+              <Navbar.Brand href="#home">Balay Nimo</Navbar.Brand>
+            </Col>
+          </Row>
+        </Container>
+      </Navbar>
+    </>
   );
 }
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
-    </div>
+    <>
+      <NavBarTop />
+
+      <Container className="mt-3">
+        <Row>
+          <Col className="border border-2" sm={4}>sm=4</Col>
+          <Col className="border border-2" sm={8}>sm=8</Col>
+        </Row>
+      </Container>
+    </>
   )
-  
 }
 
 export default App;
